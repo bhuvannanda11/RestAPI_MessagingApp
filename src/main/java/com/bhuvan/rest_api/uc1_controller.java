5aaf1985a51af756e46849d67ba9a585cf6ebe0e
bhuvannanda0011 <bhuvannanda0011@gmail.com>
2025-04-23 10:33:53 +0530

    Implemented UC1

---
+++ src/main/java/com/bhuvan/rest_api/uc1_controller.java
@@ -0,0 +1,12 @@
+package com.bhuvan.rest_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class uc1_controller {
+    @GetMapping("/123")
+    public String PrintMain(){
+        return "Hello from BridgeLabz";
+    }
+
+}
