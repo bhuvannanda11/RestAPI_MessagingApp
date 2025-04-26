c58e4e33b7d4fb0900aaeb81d8e664c1ff1a173d
bhuvannanda0011 <bhuvannanda0011@gmail.com>
2025-04-26 10:07:34 +0530

    Implemented UC4 | Check Entity_mapping.java

--- src/main/java/com/bhuvan/rest_api/UC3_RestAPICall.java
+++ src/main/java/com/bhuvan/rest_api/UC3_RestAPICall.java
@@ -1,8 +1,9 @@
 package com.bhuvan.rest_api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UC3_RestAPICall {
